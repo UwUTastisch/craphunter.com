88f16bfdb3a8ea1ee8c6b558aa925034d1848e9c
Hero_JonasHD <herojonashd@gmail.com>
2025-01-15 21:18:48 +0100

    Docker setup default pocketbase is set to pocketbase.craphunter.com

--- frontend/src/lib/components/createcrapreport-form.svelte
+++ frontend/src/lib/components/createcrapreport-form.svelte
@@ -1,13 +1,10 @@
 <script lang="ts">
+	import { pb } from '$lib/pocketbase';
+	import { userState } from '$lib/shared.svelte';
 
+	let { latitude, longitude, crapRecord } = $props();
 
-import { pb } from '$lib/pocketbase';
-import { user } from '$lib/shared.svelte';
-
-let { latitude, longitude, crapRecord} = $props();
-
-
-/*
+	/*
 crapReport data structure
 const data = {
     "user": "RELATION_RECORD_ID",
@@ -18,59 +15,55 @@ const data = {
     ]
 };
 */
-const crapReport = $state({
-    user: "RELATION_RECORD_ID",
-    status: "FIRST_SEEN",
-    description: "test",
-    tags: [
-        "l6uj93z08188k7b"
-    ]
-});
+	const crapReport = $state({
+		user: 'RELATION_RECORD_ID',
+		status: 'FIRST_SEEN',
+		description: 'test',
+		tags: ['l6uj93z08188k7b']
+	});
 
+	const crap = $state({
+		description: '', // will be updated by user input
+		latitude: latitude, // not for user input
+		longitude: longitude, // not for user input
+		crap_report: [''] // not for user input
+	});
 
-const crap = $state({
-    description: '', // will be updated by user input
-    latitude: latitude, // not for user input
-    longitude: longitude, // not for user input
-    crap_report: [""] // not for user input
-});
+	async function createCrapReport() {
+		if (!userState.user) {
+			console.log('User not logged in');
+			return;
+		}
+		crapReport.user = userState.user.userID;
+		const crapReportRecord = await pb.collection('crap_report').create(crapReport);
+		console.log('Creating crap report');
 
-async function createCrapReport() {
-    if (!user.user) {
-        console.log('User not logged in');
-        return;
-    }
-    crapReport.user = user.user.userID;
-    const crapReportRecord = await pb.collection('crap_report').create(crapReport);
-    console.log('Creating crap report');
+		if (!crapRecord) {
+			crap.crap_report = [crapReportRecord.id];
+			crap.description = crapReport.description;
+			console.log('crapRecord is null, creating crap');
 
-    if (!crapRecord) {
-        crap.crap_report = [crapReportRecord.id];
-        crap.description = crapReport.description;
-        console.log('crapRecord is null, creating crap');
-        
-        crapRecord = await pb.collection('crap').create(crap);
-        console.log('Creating crap');
-    } else {
-        crap.description = crapRecord.description;
-        crap.latitude = crapRecord.latitude;
-        crap.longitude = crapRecord.longitude;
-        crap.crap_report = [crapReportRecord.id].concat(crapRecord.crap_report);
-        console.log('crapRecord is not null, updating crap');
-        crapRecord = await pb.collection('crap').update(crapRecord.id, crap);
-    }
-}
+			crapRecord = await pb.collection('crap').create(crap);
+			console.log('Creating crap');
+		} else {
+			crap.description = crapRecord.description;
+			crap.latitude = crapRecord.latitude;
+			crap.longitude = crapRecord.longitude;
+			crap.crap_report = [crapReportRecord.id].concat(crapRecord.crap_report);
+			console.log('crapRecord is not null, updating crap');
+			crapRecord = await pb.collection('crap').update(crapRecord.id, crap);
+		}
+	}
 </script>
 
-
-{#if user.user}
-<form onsubmit="{createCrapReport}">
-    <!-- description -->
-    <div>
-        <label for="description">Description:</label>
-        <input id="description" type="text" bind:value={crapReport.description} />
-    </div>
-    <!-- Tags 
+{#if userState.user}
+	<form onsubmit={createCrapReport}>
+		<!-- description -->
+		<div>
+			<label for="description">Description:</label>
+			<input id="description" type="text" bind:value={crapReport.description} />
+		</div>
+		<!-- Tags 
     {#if crapRecord}
         <div>
             <label for="tags">Tags:</label>
@@ -83,9 +76,9 @@ async function createCrapReport() {
     {/if}
     <button type="button">Add Tag</button>
     -->
-    <!-- Submit -->
-    <button onclick={createCrapReport} type="submit">Submit</button>
-</form>
+		<!-- Submit -->
+		<button onclick={createCrapReport} type="submit">Submit</button>
+	</form>
 {:else}
-    <h1>Log in to create a crap report</h1>
+	<h1>Log in to create a crap report</h1>
 {/if}
--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -2,42 +2,46 @@
 	import '../app.css';
 	import NavBar from '$lib/components/nav-bar.svelte';
 	import ShellLayout from '$lib/components/shell-layout.svelte';
-  import { restoreAuthFromCookie, restoreLocationFromCookie, currentUser, pb} from '$lib/pocketbase';
-  import { onMount } from 'svelte';
+	import {
+		restoreAuthFromCookie,
+		restoreLocationFromCookie,
+		currentUser,
+		pb
+	} from '$lib/pocketbase';
+	import { onMount } from 'svelte';
 
-  import { userState } from "$lib/shared.svelte.js";
+	import { userState } from '$lib/shared.svelte.js';
 
-  onMount(() => {
-        restoreAuthFromCookie();
-        restoreLocationFromCookie();
-  });
+	export const prerender = true;
+	export const ssr = true;
 
+	onMount(() => {
+		restoreAuthFromCookie();
+		restoreLocationFromCookie();
+	});
 
-  currentUser.subscribe
-  (value => {
-    userState.user = !value ? null : { userID: value.id, user: value.name, nickname: null };
-    console.log("user",userState?.user);
+	currentUser.subscribe((value) => {
+		userState.user = !value ? null : { userID: value.id, user: value.name, nickname: null };
+		console.log('user', userState?.user);
 
-    // after the above you can also access the auth data from the authStore
-    console.log("is valid" ,pb.authStore.isValid);
-    console.log("token",pb.authStore.token);
-    if (pb.authStore.record) {
-      console.log("record", pb.authStore.record.id);
-    } else {
-      console.log("no record");
-    }
-  });
+		// after the above you can also access the auth data from the authStore
+		console.log('is valid', pb.authStore.isValid);
+		console.log('token', pb.authStore.token);
+		if (pb.authStore.record) {
+			console.log('record', pb.authStore.record.id);
+		} else {
+			console.log('no record');
+		}
+	});
+</script>
 
-  </script>
-  
-  <!-- Global Nav -->
-  <NavBar user={userState.user}/>
-  
-  <!--
+<!-- Global Nav -->
+<NavBar user={userState.user} />
+
+<!--
 	Our shell layout wraps the page content but includes a side/bottom panel for login or info.
 	We pass the page <slot /> into the shell layout so the page content isn't overwritten.
   -->
-  <ShellLayout user={userState.user}>
+<ShellLayout user={userState.user}>
 	<slot />
-  </ShellLayout>
-  
+</ShellLayout>
